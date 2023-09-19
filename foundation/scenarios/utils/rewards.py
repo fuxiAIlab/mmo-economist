80979a9aefc5f21d2924c2dfb4d69c3d65b7e492
Shiwei Zhao <zhaoshiwei@corp.netease.com>
2023-09-19 15:11:50 +0800

    add(*): new rewards for planner

--- foundation/scenarios/utils/rewards.py
+++ foundation/scenarios/utils/rewards.py
@@ -45,3 +45,41 @@ def utility_for_planner(monetary_incomes, nonmonetary_incomes,
     equality = equality_weight * social_metrics.get_equality(
         nonmonetary_incomes) + (1 - equality_weight)
     return equality * profitability
+
+
+def utility2_for_planner(monetary_incomes, nonmonetary_incomes,
+                         equality_weight):
+    n_agents = len(monetary_incomes)
+    # 人均付费 ARPU
+    profitability = social_metrics.get_profitability(
+        monetary_incomes) / n_agents
+    # 人均公平性加权战力（人均战力*公平性）
+    equality = (
+        equality_weight * social_metrics.get_equality(nonmonetary_incomes) +
+        (1 - equality_weight)) * (np.sum(nonmonetary_incomes) / n_agents)
+    return equality * profitability
+
+
+def utility_normalized_for_planner(monetary_incomes, exp_monetary_incomes,
+                                   nonmonetary_incomes, equality_weight):
+    n_agents = len(monetary_incomes)
+    profitability = social_metrics.get_profitability(
+        monetary_incomes) / n_agents / exp_monetary_incomes
+    equality = equality_weight * social_metrics.get_equality(
+        nonmonetary_incomes) + (1 - equality_weight)
+    return equality * profitability
+
+
+def utility2_normalized_for_planner(monetary_incomes, exp_monetary_incomes,
+                                    nonmonetary_incomes,
+                                    exp_nonmonetary_incomes, equality_weight):
+    n_agents = len(monetary_incomes)
+    # 人均付费 ARPU / 期望人均付费 exp ARPU
+    profitability = social_metrics.get_profitability(
+        monetary_incomes) / n_agents / exp_monetary_incomes
+    # 人均公平性加权战力（人均战力*公平性）/ 期望人均战力
+    equality = (
+        equality_weight * social_metrics.get_equality(nonmonetary_incomes) +
+        (1 - equality_weight)) * (np.sum(nonmonetary_incomes) /
+                                  n_agents) / exp_nonmonetary_incomes
+    return equality * profitability
